8e163415c66c820e6e94d512d6f6dbe4cc9b595c
KamilSzus <kamil-szustak1@wp.pl>
2022-04-01 22:19:00 +0200

    now can change fragmet once :D

--- app/src/main/java/com/example/aplikacja_pogodowa/DayFragment.java
+++ app/src/main/java/com/example/aplikacja_pogodowa/DayFragment.java
@@ -1,12 +1,16 @@
 package com.example.aplikacja_pogodowa;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 public class DayFragment extends Fragment {
 
@@ -15,17 +19,9 @@ public class DayFragment extends Fragment {
 
     private String mParam1;
     private String mParam2;
+    private GestureDetector mDetector;
 
     public DayFragment() {
-    }
-
-    public static DayFragment newInstance(String param1, String param2) {
-        DayFragment fragment = new DayFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
@@ -40,7 +36,66 @@ public class DayFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_day, container, false);
+        View view = inflater.inflate(R.layout.fragment_day, container, false);
+        mDetector = new GestureDetector(getActivity(), new SimpleOnGestureListener(){
+            @Override
+            public boolean onDown(MotionEvent event){
+
+                return true;
+            }
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                boolean result = false;
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    final int SWIPE_THRESHOLD = 100;
+                    final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffX > 0) {
+                            } else {
+                            }
+                            result = false;
+                        }
+                    }
+                    else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            System.out.println("gora dol");
+                            requireActivity()
+                                    .getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.test,new HoursFragment())
+                                    .commit();
+                        } else {
+                            requireActivity()
+                                    .getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.test,new DayFragment())
+                                    .commit();
+                            System.out.println("dol");
+                        }
+                        result = true;
+                    }
+                }
+                catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+                return result;
+            }
+        });
+        view.setOnTouchListener(touchListener);
+
+        return view;
     }
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @SuppressLint("ClickableViewAccessibility")
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            return mDetector.onTouchEvent(event);
+
+        }
+    };
 }
--- app/src/main/java/com/example/aplikacja_pogodowa/HoursFragment.java
+++ app/src/main/java/com/example/aplikacja_pogodowa/HoursFragment.java
@@ -1,58 +1,89 @@
 package com.example.aplikacja_pogodowa;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
-import android.view.VelocityTracker;
-import android.widget.Toast;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class HoursFragment extends AppCompatActivity {
+public class HoursFragment extends Fragment {
 
-    private VelocityTracker mVelocityTracker = null;
-
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private GestureDetector mDetector;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hours_fragment);
-
+        if (getArguments() != null) {
+            String mParam1 = getArguments().getString(ARG_PARAM1);
+            String mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int index = event.getActionIndex();
-        int action = event.getActionMasked();
-        int pointerId = event.getPointerId(index);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_hours, container, false);
+        mDetector = new GestureDetector(getActivity(), new GestureDetector.SimpleOnGestureListener(){
 
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                Toast.makeText(this, "down", Toast.LENGTH_LONG).show();
-                if (mVelocityTracker == null) {
-                    mVelocityTracker = VelocityTracker.obtain();
-                } else {
-                    mVelocityTracker.clear();
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                boolean result = false;
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    final int SWIPE_THRESHOLD = 100;
+                    final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffX > 0) {
+                            } else {
+                            }
+                            result = false;
+                        }
+                    }
+                    else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            System.out.println("gora dol");
+                            requireActivity()
+                                    .getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.test,new HoursFragment())
+                                    .commit();
+                        } else {
+                            requireActivity()
+                                    .getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.test,new DayFragment())
+                                    .commit();
+                            System.out.println("dol");
+                        }
+                        result = true;
+                    }
                 }
-                mVelocityTracker.addMovement(event);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                Toast.makeText(this, "eeee", Toast.LENGTH_LONG).show();
-                mVelocityTracker.addMovement(event);
-                mVelocityTracker.computeCurrentVelocity(1000);
-                Log.d("", "X velocity: " + mVelocityTracker.getXVelocity(pointerId));
-                Log.d("", "Y velocity: " + mVelocityTracker.getYVelocity(pointerId));
-                break;
-            case MotionEvent.ACTION_UP:
-                Toast.makeText(this, "up", Toast.LENGTH_LONG).show();
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                mVelocityTracker.recycle();
-                break;
-        }
-        return true;
+                catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+                return result;
+            }
+        });
+        view.setOnTouchListener(touchListener);
+
+        return view;
     }
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @SuppressLint("ClickableViewAccessibility")
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
 
-
-
+            return mDetector.onTouchEvent(event);
+        }
+    };
 }
