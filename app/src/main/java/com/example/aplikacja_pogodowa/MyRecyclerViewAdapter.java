c9bc68dca6e99f7e49c616543193fc1e4005bf6d
KamilSzus <69347040+KamilSzus@users.noreply.github.com>
2022-05-05 09:01:41 +0200

    add button to end of viewPager

--- app/src/main/java/com/example/aplikacja_pogodowa/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/aplikacja_pogodowa/MyRecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package com.example.aplikacja_pogodowa;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,33 +23,45 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.hours_row, parent, false);
+        View view;
+        if(viewType == R.layout.hours_row) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.hours_row, parent, false);
+        }else{
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.button, parent, false);
+        }
         view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
         int height = view.getMeasuredHeight();
         return new ViewHolder(view, height);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.myTextViewHours.setText(mData.get(position));
-        holder.myTextViewTemperature.setText(mData.get(position));
-        holder.imageView.setImageResource(R.drawable.temp);
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if (position == mData.size()) {
+            holder.button.setOnClickListener(v -> {
+            });
+        } else {
+            holder.myTextViewHours.setText(mData.get(position));
+            holder.myTextViewTemperature.setText(mData.get(position));
+            holder.imageView.setImageResource(R.drawable.temp);
+        }
+
     }
 
     @Override
     public int getItemCount() {
-        return mData.size();
+        return mData.size()+1;
     }
 
     @Override
     public int getItemViewType(final int position) {
-        return R.layout.hours_row;
+        return (position == mData.size()) ? R.layout.button : R.layout.hours_row;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder  {
         TextView myTextViewTemperature;
         TextView myTextViewHours;
         ImageView imageView;
+        Button button;
 
         ViewHolder(View itemView, int height) {
             super(itemView);
@@ -56,6 +69,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
             myTextViewTemperature = itemView.findViewById(R.id.temp3);
             myTextViewHours = itemView.findViewById(R.id.hours);
             imageView = itemView.findViewById(R.id.WeatherIcon);
+            button = itemView.findViewById(R.id.back);
         }
     }
 
